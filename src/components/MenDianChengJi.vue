799a125b4a3866e9bb5b010b4dc4c669df8f5583
盛凯 <80961244@yonghui.cn>
2019-11-27 09:56:02 +0800

    top5柱状图

--- src/components/MenDianChengJi.vue
+++ src/components/MenDianChengJi.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <div class="box" :style="{width: '450px', height: '400px',margin:'0',padding:'0'}">
-      <div id="myChart1"></div>
-    </div>
-    <div class="box" id="myChart2" :style="{width: '450px', height: '400px',margin:'0',padding:'0'}"></div>
+    <div id="myChart1" :style="{width: '450px', height: '400px',margin:'0',padding:'0'}"></div>
+    <div id="myChart2" :style="{width: '450px', height: '400px',margin:'0',padding:'0'}"></div>
   </div>
 </template>
 
@@ -28,42 +26,103 @@
         // 基于准备好的dom，初始化echarts实例
         let myChart1 = echarts.init(document.getElementById('myChart1'), 'dark');
         myChart1.setOption({
-          backgroundColor:'#FFFFFF',
+          backgroundColor:'#0B284C',
           title: {
             text: '门店成绩TOP5'
           },
           grid: {
             left: '3%',
             right: '4%',
-            bottom: '3%',
-            containLabel: true
+            bottom: '10%',
           },
           xAxis: [{
+            axisTick: {
+              show: false,
+            },
+            axisLine: {
+              show: false
+            },
             type: 'category',
-            data: ['1月','2月','3月','4月','5月']
+            data: ['象园店','屏西店','华林店','鼓楼店','黎明店']
           }],
           yAxis: {
             show: false
           },
           series: [{
-            name: '检查企业数',
+            name: '门店成绩',
             type: 'bar',
             itemStyle:{
-              normal:{color:'#01949B'},
+              normal:{color:'#F8BB52'},
+            },
+            label: {
+              show: true,
+              position: 'inside',
+              textStyle: {
+                color: '#fff',
+                fontSize: 20
+              }
+            },
+            markPoint : {
+              data: [
+                {type: 'max', name: '最大值'}
+              ]
             },
             data: [2031, 1793, 3640, 2593, 4377]
           }]
         })
       },
       myChart2() {
-
+        // 基于准备好的dom，初始化echarts实例
+        let myChart2 = echarts.init(document.getElementById('myChart2'), 'dark');
+        myChart2.setOption({
+          backgroundColor:'#0B284C',
+          title: {
+            text: '门店成绩BOTTOM5'
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '10%',
+          },
+          xAxis: [{
+            axisTick: {
+              show: false,
+            },
+            axisLine: {
+              show: false
+            },
+            data: ['象园店','屏西店','华林店','鼓楼店','黎明店']
+          }],
+          yAxis: {
+            show: false
+          },
+          series: [{
+            name: '门店成绩',
+            type: 'bar',
+            itemStyle:{
+              normal:{color:'#AE5557'},
+            },
+            label: {
+              show: true,
+              position: 'inside',
+              textStyle: {
+                color: '#fff',
+                fontSize: 20
+              }
+            },
+            markPoint : {
+              data: [
+                {type: 'min', name: '最小值'}
+              ]
+            },
+            data: [2031, 1793, 3640, 2593, 4377]
+          }]
+        })
       }
     }
   }
 </script>
 
 <style scoped>
-  .box{
-    display: inline-block;
-  }
+
 </style>
